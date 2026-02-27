bed6f60fdd0565ac39467336b82cd96d81475457
Dusabe-s <marierosedusabe58@gmail.com>
2026-02-27 12:23:14 +0200

    initialization spring boot backend

---
+++ AcademicVerification_Backend/src/main/java/com/dusabe/AcademicVerificationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dusabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicVerificationBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademicVerificationBackendApplication.class, args);
+    }
+
+}
---
+++ AcademicVerification_Backend/src/test/java/com/dusabe/AcademicVerificationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dusabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicVerificationBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
